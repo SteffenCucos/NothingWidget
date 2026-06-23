8fd2e993824fa9585c793a3e89c4f8a4e213551c
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-06-22 21:54:42 -0400

    Build first functional widget app

--- app/src/main/java/com/steffencucos/nothingwidget/solar/SolarEvent.kt
+++ app/src/main/java/com/steffencucos/nothingwidget/solar/SolarEvent.kt
@@ -3,5 +3,8 @@ package com.steffencucos.nothingwidget.solar
 data class SolarEvent(
     val label: String,
     val displayTime: String,
-    val progressPercent: Int
+    val timeRemaining: String,
+    val progressPercent: Int,
+    val iconText: String,
+    val statusText: String
 )
