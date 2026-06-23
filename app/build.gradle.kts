6579c2c6aeabcf83a9fddf1c176f7d374e0a8630
SteffenCucos <40307614+SteffenCucos@users.noreply.github.com>
2026-06-22 21:54:30 -0400

    Build first functional widget app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 }
